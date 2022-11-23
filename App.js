e1e8ee445a11b6e212ff5ee3ea244e3cf8de4bf3
tayyab jamil <50493469+tayyabjamil@users.noreply.github.com>
2022-11-23 22:22:45 +0500

    latest main

--- App.js
+++ App.js
@@ -6,14 +6,7 @@ export default class App extends Component {
     return (
       <View>
         <Text>main</Text>
-        <Text>Branch 2</Text>
-        <Text>Branch 3 first changes</Text>
-        <Text>branch 4 changes</Text>
-        <Text>branch 5 changes</Text>
-        <Text>branch 6 changes</Text>
-        <Text>branch 6 1 changes</Text>
-        <Text>branch 6 2 changes</Text>
-     
+       
       </View>
     )
   }
