c4845b6f36f7c410c46b52259a051215abe6fbc1
tayyab jamil <50493469+tayyabjamil@users.noreply.github.com>
2022-11-23 22:10:49 +0500

    branch 3 changes

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <View>
+        <Text>Branch 2</Text>
         <Text>Branch 3 first changes</Text>
       </View>
     )
